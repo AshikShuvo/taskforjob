b6bbfdb08777945625ddf90abf376b267c8ae5f3
AshikShuvo <ashikshuvo1996@gmail.com>
2020-07-12 21:57:38 +0600

    draft touch

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("hello world"));
 app.use("/api/users", require("./routes/users"));
-app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
